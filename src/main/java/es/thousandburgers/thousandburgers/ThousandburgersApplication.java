237e19013dc0c6b00e55646fffb691caa1781268
jose260899 <a035596971d@ausiasmarch.net>
2023-12-23 20:30:46 +0100

    primer commit

---
+++ src/main/java/es/thousandburgers/thousandburgers/ThousandburgersApplication.java
@@ -0,0 +1,13 @@
+package es.thousandburgers.thousandburgers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThousandburgersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThousandburgersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/thousandburgers/thousandburgers/ThousandburgersApplicationTests.java
@@ -0,0 +1,13 @@
+package es.thousandburgers.thousandburgers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThousandburgersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
